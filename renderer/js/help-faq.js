38e35151e1e4ed2f03ebe61fdda058af61bd75f2
Jeremy Mora <qjcbmora@gmail.com>
2024-07-07 03:27:00 +0800

    v0.9 Updated about module Updated user manual content

---
+++ renderer/js/help-faq.js
@@ -0,0 +1,30 @@
+const faqData = [
+	[
+		'Q: How can I create a sales record?',
+		'A: Sales records can be created by going to the point of sales module, and clicking the "+ Create Sales Record" button at the top-right corner of the screen. In the sales record creation screen, you must fill up the needed details of the sales record before clicking the submit button to create the sales record.'
+	],
+	[
+		'Q: How do I update the inventory when receiving new stock?',
+		'A: When receiving a new stock of items, an inventory record must be created. An inventory record can be created by going to the inventory management module and clicking the "+ Create Inventory Record" button at the top-right corner of the screen. In the inventory record creation, you must fill up the necessary details, and most importantly set the sign to "(+) Gain/Restock" when setting the amount.'
+	],
+	[
+		'Q: Can I edit customer information after registration?',
+		'A: Yes, customer information can be edited in the customer management module after registration. This is done by either (1) selecting the customer you want to edit from the customer table and clicking the "Edit Customer" button, or by (2) clicking the "Edit Customer" button, inputting the customer id and clicking "Check ID" before editing.'
+	],
+	[
+		'Q: How can I recover a forgotten password?',
+		'A: Passwords can be reset by going through the forgot password module or by having it set by the admin. In the login screen, you can click the "Forgot password?" link to proceed to the forgot password procedure. If you are a staff, you can also ask help from an admin to have your password reset through the user management module.'
+	],
+	[
+		'Q: What permissions do Admin and Staff have?',
+		'A: In the system, the users with staff permissions are capable of registering customers, editing customers, creating sales records, and creating inventory records, while users with admin permissions are able to the same things and more. Users with admin permissions are able to edit sales and inventory records and services, as well as set maintenance-related settings in the system and generate reports.'
+	],
+	[
+		'Q: Why can\'t I edit a sales or inventory record?',
+		'A: Editing sales and inventory records can only be done by admins. In case a record needs to be edited, please inform an admin to perform the edit.'
+	],
+	[
+		'Q: How do I add more choices for the services in a sales record?',
+		'A: The services available when creating sales records can be set by going to the point of sales module and clicking the "Edit Services" button. In the service editing screen, you may edit services and add (but not remove) services to be used in sales records.'
+	],
+]
